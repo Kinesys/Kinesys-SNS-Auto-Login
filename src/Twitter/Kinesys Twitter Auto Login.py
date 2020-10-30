11462001c4865e4be9ec1f0e4afe591d98c00b25
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-10-30 14:51:13 +0900

    Create Kinesys Twitter Auto Login.py

---
+++ src/Twitter/Kinesys Twitter Auto Login.py
@@ -0,0 +1,21 @@
+#Kinesys Twitter Auto Login.py
+import time
+from myid import ID, PW
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get('https://twitter.com/')
+
+time.sleep(3) #대기 시간(3)
+
+#로그인 구문
+login_id = driver.find_element_by_name('user id')
+login_id.send_keys(ID)
+login_pw =driver.find_element_by_name('user pw')
+login_pw.send_keys(PW)
+login_pw_send_keys(Keys.RETURN)
+
+time.sleep(3)
